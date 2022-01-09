a953156156a1cc3629f3c6b52836c5a45e82a321
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2022-01-09 19:53:34 -0300

    Created basic structures and components for use

---
+++ src/views/examples/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const About = props => (
+    <div className="About">
+        <h1>Componente About </h1>
+        <p>Aqui tem um about.</p>
+    </div>
+)
+
+export default About
---
+++ src/views/examples/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = props => (
+    <div className="Home">
+        <h1>Componente Home </h1>
+        <h2>Componente Home </h2>
+        <p>Bem-vindo(a)!</p>
+    </div>
+)
+
+export default Home
