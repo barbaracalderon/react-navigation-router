be041a1727ea8c610de3bce229d4282364298a87
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2022-01-09 20:39:15 -0300

    Added ids to URL address, now they're dynamic

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -3,14 +3,18 @@ import './Content.css'
 import { Routes, Route } from 'react-router-dom'
 import About from '../../views/examples/About'
 import Home from '../../views/examples/Home'
+import Param from '../../views/examples/Param'
 
 const Content = props => (
     <main className="Content">
         <Routes>
-            <Route path="/about" element={ <About /> } />
+            <Route path="/home" element={ <Home /> } />
         </Routes>
         <Routes>
-            <Route path="/home" element={ <Home /> } />
+            <Route path="/param/:id" element={ <Param /> } />
+        </Routes>
+        <Routes>
+            <Route path="/about" element={ <About /> } />
         </Routes>
 
     </main>
--- src/components/layout/Menu.jsx
+++ src/components/layout/Menu.jsx
@@ -2,15 +2,22 @@ import React from 'react'
 import './Menu.css'
 import { Link } from 'react-router-dom'
 
+
 const Menu = props => (
     <div className="Menu">
         <nav>
             <ul>
                 <li>
-                    <Link to="/Home">Início</Link>
+                    <Link to="/home">Início</Link>
                 </li>
                 <li>
-                    <Link to="/About">Sobre</Link>
+                    <Link to="/param/123">Param #01</Link>
+                </li>
+                <li>
+                    <Link to="/param/legal">Param #02</Link>
+                </li>                
+                <li>
+                    <Link to="/about">Sobre</Link>
                 </li>
             </ul>
         </nav>
